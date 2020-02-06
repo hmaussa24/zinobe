f414c88c32baffb1b3cff71755696a14ec47dfa7
harold <haroldmaussa@gmail.com>
2020-02-06 10:04:48 -0500

    Integracion de plnatillas Twig

--- resource/views/registrar.php
+++ resource/views/registrar.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nuevo Usuario</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
+{% extends "plantilla.php" %}
+
+{% block title %}Registrar{% endblock %}
+{% block head %}
+    {{ parent() }}
+{% endblock %}
+{% block content %}
+
 <div class="container" style="margin-top: 25px">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -83,5 +81,4 @@
         </div>
     </div>
 </div>
-</body>
-</html>
+{% endblock %}
