e19a0dd346b1d7a0a78014ad0c97dd5665a8b81d
harold <haroldmaussa@gmail.com>
2020-02-06 22:15:29 -0500

    consulta base de datos externa

---
+++ app/Clases/CustomerData.php
@@ -0,0 +1,53 @@
+<?php
+
+class CustomerData{
+   private  $curl;
+
+   function __construct() {
+    $this->$curl = curl_init();
+   }
+
+   function consultarSisben($documento){
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.misdatos.com.co/api/co/sisben/consultarPuntaje",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "documentType=CC&documentNumber=".$documento,
+        CURLOPT_HTTPHEADER => array(
+          "Authorization: 6gz9hpl6rnlgkkedz9jcs6is83u409mu",
+          "Content-Type: application/x-www-form-urlencoded"
+        ),
+      ));
+
+      $response = curl_exec($curl);
+      curl_close($curl);
+      return $response;
+   }
+
+   function consultraConductor($documento){
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.misdatos.com.co/api/co/runt/consultarConductor",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "documentType=CC&documentNumber=".$documento,
+        CURLOPT_HTTPHEADER => array(
+          "Authorization: 6gz9hpl6rnlgkkedz9jcs6is83u409mu",
+          "Content-Type: application/x-www-form-urlencoded"
+        ),
+      ));
+      
+      $response = curl_exec($curl);
+      curl_close($curl);
+      return $response;
+   }
+}
